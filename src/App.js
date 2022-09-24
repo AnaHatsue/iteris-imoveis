a0c9e2f2e200df8e776171f8f635d927ce529ae9
Ana Hatsue Martinhão Esquinelato <anahatsue.dev@outlook.com>
2022-09-24 00:02:57 -0300

    Iniciado projeto Iteris imoveis.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SobrePage from "./pages/SobrePage";
+
+function App() {
+  return (
+    <div className="App">
+      <SobrePage></SobrePage>
+      
+    </div>
+  );
+}
+
+export default App;
