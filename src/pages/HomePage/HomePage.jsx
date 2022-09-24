9753f1047e2252db2b4a87f79a97b67f410a7dbd
Ana Hatsue Martinhão Esquinelato <anahatsue.dev@outlook.com>
2022-09-24 03:31:00 -0300

    Adicionado Slide de imóveis a tela inicial

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,10 @@
+import SlideShow from '../../components/SlideShow/'
+
 function HomePage() {
     return ( 
         <div>
             <h1>Iteris Imóveis</h1>
+            <SlideShow/>
         </div>
      );
 }
